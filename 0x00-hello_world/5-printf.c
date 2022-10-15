b5a3250e655d87c173fa6f42e99803d9d2ff1828
Omolola <106765925+Omolola9@users.noreply.github.com>
2022-10-15 23:34:58 +0100

    low level

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Return: Always 0
  */
-int masin(void)
+int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
