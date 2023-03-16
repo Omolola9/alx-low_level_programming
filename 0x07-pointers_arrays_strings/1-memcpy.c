3ac7f3d82c0e444cca6618cf94ba898ab08dd305
Omolola <106765925+Omolola9@users.noreply.github.com>
2023-03-17 00:04:24 +0100

    more pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,21 @@
-gdhjjj
+#include "main.h"
+
+/**
+ * _memcpy -  copies memory area.
+ * @dest: copy to.
+ * @src: copy from.
+ * @n: number of bytes copy from @src.
+ *
+ * Return: a pointer to dest
+ */
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
