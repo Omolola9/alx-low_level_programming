add153d7628c2e551e743afcd2fd529b11cd2cc8
Omolola <106765925+Omolola9@users.noreply.github.com>
2023-03-17 00:10:33 +0100

    more pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,20 @@
-bkmb
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: a pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
